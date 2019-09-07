9d07cd4ae4d697b3898c66e301810599da383df4
LinxOK <linxok@gmail.com>
2019-09-07 20:05:03 +0300

    ok. form 2 ready.

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-document.getElementById('form_first').addEventListener('click', (event) => {
+document.getElementById('sign_up').addEventListener('click', (event) => {
     let form_first = document.getElementById('form_first');
     if (form_first.checkValidity()) {
         document.getElementById('form_first').style.display = 'none';
@@ -8,7 +8,7 @@ document.getElementById('form_first').addEventListener('click', (event) => {
     }
 });
 
-document.getElementById('form_second').addEventListener('click', (event) => {
+document.getElementById('buttonSave').addEventListener('click', (event) => {
     let form_second = document.getElementById('form_second');
     if (form_second.checkValidity()) {
         document.getElementById('form_first').style.display = 'none';

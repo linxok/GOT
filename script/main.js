ee8586f5deb483b5c03d6000d288e8060da23c08
LinxOK <linxok@gmail.com>
2019-09-20 21:05:25 +0300

    исправления после второго ревью

--- script/main.js
+++ script/main.js
@@ -1,19 +1,19 @@
 document.getElementById('sign_up').addEventListener('click', (event) => {
-    let form_first = document.getElementById('form_first');
-    if (form_first.checkValidity()) {
-        document.getElementById('form_first').style.display = 'none';
-        document.getElementById('form_second').style.display = 'flex';
+  let form_first = document.getElementById('form_first');
+  if (form_first.checkValidity()) {
+    document.getElementById('form_first').style.display = 'none';
+    document.getElementById('form_second').style.display = 'flex';
 
-      event.preventDefault(); // https://learn.javascript.ru/default-browser-action
-    }
+    event.preventDefault(); // https://learn.javascript.ru/default-browser-action
+  }
 });
 
 document.getElementById('buttonSave').addEventListener('click', (event) => {
-    let form_second = document.getElementById('form_second');
-    if (form_second.checkValidity()) {
-        document.getElementById('form_first').style.display = 'none';
-        document.getElementById('form_second').style.display = 'flex';
+  let form_second = document.getElementById('form_second');
+  if (form_second.checkValidity()) {
+    document.getElementById('form_first').style.display = 'none';
+    document.getElementById('form_second').style.display = 'flex';
 
-      event.preventDefault();
-    }
+    event.preventDefault();
+  }
 });
